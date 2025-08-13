9dcdcb1160c45c8dc464c7937aebd0e4977114fa
adilet <adilet.dildeev08@gmail.com>
2025-08-13 17:50:52 +0600

    s

--- lib/text_styles/title_theme.dart
+++ lib/text_styles/title_theme.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class TextStyles {
   static TextStyle blackTitle(BuildContext context) =>
       Theme.of(context).textTheme.bodyLarge!.copyWith(
         fontSize: 17,
         fontWeight: FontWeight.w700,
-        fontFamily: GoogleFonts.nunito().fontFamily,
+        fontFamily: 'Nunito',
       );
 
   static TextStyle redTitle(BuildContext context) =>
@@ -14,7 +13,7 @@ class TextStyles {
         fontSize: 17,
         // color: Colors.orange,
         fontWeight: FontWeight.bold,
-        fontFamily: GoogleFonts.nunito().fontFamily,
+        fontFamily: 'Nunito',
       );
 
   static TextStyle largeTitle(BuildContext context) =>
@@ -22,60 +21,64 @@ class TextStyles {
         fontSize: 21,
         // color: Colors.orange,
         fontWeight: FontWeight.bold,
-        fontFamily: GoogleFonts.nunito().fontFamily,
+        fontFamily: 'Nunito',
       );
   static TextStyle salavatTitle(BuildContext context) =>
       Theme.of(context).textTheme.bodyLarge!.copyWith(
         // color: Colors.black,
         fontSize: 20,
         fontWeight: FontWeight.bold,
-        fontFamily: GoogleFonts.nunito().fontFamily,
+        fontFamily: 'Nunito',
       );
   static TextStyle arabicTitle(BuildContext context) =>
       Theme.of(context).textTheme.bodyLarge!.copyWith(
         // color: Colors.black,
         fontSize: 21,
         fontWeight: FontWeight.bold,
-        fontFamily: GoogleFonts.nunito().fontFamily,
+        fontFamily: 'Nunito',
       );
   static TextStyle sunushTitle(BuildContext context) =>
       Theme.of(context).textTheme.bodySmall!.copyWith(
         // color: Colors.black,
         fontSize: 21,
         fontWeight: FontWeight.bold,
-        fontFamily: GoogleFonts.nunito().fontFamily,
+        fontFamily: 'Nunito',
       );
   static TextStyle sunushTitle2(BuildContext context) =>
       Theme.of(context).textTheme.bodySmall!.copyWith(
         // color: Colors.black,
         fontSize: 17,
         fontWeight: FontWeight.bold,
-        fontFamily: GoogleFonts.nunito().fontFamily,
+        fontFamily: 'Nunito',
       );
 }
 // import 'package:flutter/material.dart';
 // import 'package:google_fonts/google_fonts.dart';
 
 class TextStyless {
-  static final TextStyle redTitle = GoogleFonts.nunito(
+  static final TextStyle redTitle = TextStyle(
     fontSize: 17,
     fontWeight: FontWeight.bold,
     color: Colors.red,
+    fontFamily: 'Nunito',
   );
-  static final TextStyle blackTitle = GoogleFonts.nunito(
+  static final TextStyle blackTitle = TextStyle(
     fontSize: 17,
     fontWeight: FontWeight.w700,
     color: Colors.black,
+    fontFamily: 'Nunito',
   );
-  static final TextStyle largeTitle = GoogleFonts.nunito(
+  static final TextStyle largeTitle = TextStyle(
     fontSize: 21,
     fontWeight: FontWeight.bold,
     color: Colors.red,
+    fontFamily: 'Nunito',
   );
-  static final TextStyle salavatTitle = GoogleFonts.nunito(
+  static final TextStyle salavatTitle = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.bold,
     color: Colors.black,
+    fontFamily: 'Nunito',
   );
 }
 
